ad886de3830a75ce5a8eb5506e45606a81f19019
sally <sallyakrum83@gmail.com>
2019-02-19 12:16:54 -0600

    install spotify api

--- keys.js
+++ keys.js
@@ -5,4 +5,8 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
+ 
 var spotify = new Spotify(keys.spotify);
+  
--- liri.js
+++ liri.js
@@ -23,3 +23,5 @@ require("dotenv").config();
 // The song's name
 // A preview link of the song from Spotify
 // The album that the song is from
+
+console.log()
