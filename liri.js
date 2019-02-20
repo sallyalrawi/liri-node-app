48b77e8c112e98c5aa6831b32a73e2d4737a7e5b
sally <sallyakrum83@gmail.com>
2019-02-20 09:26:02 -0600

    added some changes

--- liri.js
+++ liri.js
@@ -1,27 +1,17 @@
-require("dotenv").config();
+// var dotenv = require("dotenv").config();
 // Make it so liri.js can take in one of the following commands:
+// var keys = require("./keys.js");
+// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
+var axios = require("axios");
 
-// 1)concert-this
-//2) spotify-this-song
-//3) movie-this
-//4) do-what-it-says
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("  http://www.omdbapi.com/?i=tt3896198&apikey=edfc2147").then(
+  function(response) {
+    console.log("The movie's Title is: " + response.data);
+  },
+  function(error){
+      console.log('error');
+  }
 
-// What Each Command Should Do:
-// A)__> $node liri.js concert-this <artist/band name here>
+);
 
-// This will search the Bands in Town Artist Events API ("https:// https://api.spotify.com/artists/" + artist + "/events?app_id=f3a4147086884cd48a4a4ca1e5a1955b") for an artist and render the following information about each event to the terminal:
-
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-//  B)__> $node liri.js spotify-this-song '<song name here>'
-// This will show the following information about the song in your terminal/bash window
-
-
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-
-console.log()
