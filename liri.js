fef9c61ae8dac839c1fcb4d1b9b81ee4f5255f74
sally <sallyakrum83@gmail.com>
2019-02-20 21:56:14 -0600

    added the moment js

--- liri.js
+++ liri.js
@@ -1,17 +1,22 @@
  require("dotenv").config();
 // Make it so liri.js can take in one of the following commands:
 var Spotify = require('node-spotify-api');
+
 var fs = require('fs');
 
+var moment = require('moment');
+
  var keys = require("./keys.js");
+
  var spotify = new Spotify(keys.spotify);
+
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
 var axios = require("axios");
 var movieName = process.argv[3];
 // Then run a request with axios to the OMDB API with the movie specified
 axios.get("  http://www.omdbapi.com/?t=" + movieName +"&plot=full&tomatoes=true&apikey=edfc2147").then(
   function(response) {
-    console.log("The movie's Title is: " + JSON.stringify(response.data));
+    //console.log("The movie's Title is: " + JSON.stringify(response.data));
   },
   function(error){
       console.log('error');
@@ -23,7 +28,8 @@ var artist = process.argv[3];
 
 axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
   function(response) {
-    console.log("The movie's Title is: " + JSON.stringify(response.data));
+    console.log("The concert information: " + JSON.stringify(response.data[0].datetime));
+    console.log(moment(response.data[0].datetime).format('LLLL'));
   },
   function(error){
       console.log('error');
