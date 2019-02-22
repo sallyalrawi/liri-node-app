ac3792fc41517375f0eea1fca71e51f4f8fc0e3d
sally <sallyakrum83@gmail.com>
2019-02-22 13:23:33 -0600

    added images folder

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # liri-node-app
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+-LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+-when you input the song name the output should be the information about this song and links.
+-when you input the artist name and band concert the output will show all information about that artist and the concert time and the last updates.
+-when you input the movie name the output will be all the information about that movie.
+
+-HERE IS SCREENSHOT OF THE OUTPUTS:
+![artist](/images/artist.png)
--- liri.js
+++ liri.js
@@ -1,88 +1,80 @@
+ require("dotenv").config();
+
+var Spotify = require('node-spotify-api');
+
 require("dotenv").config();
 
-var Spotify = require("node-spotify-api");
+var Spotify = require('node-spotify-api');
 
-require("dotenv").config();
+var fs = require('fs');
 
-var Spotify = require("node-spotify-api");
-
-var fs = require("fs");
-
-var moment = require("moment");
+var moment = require('moment');
 
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
+
 function searchSpotify(cb) {
   var songName = process.argv[3];
-  console.log("got here");
-  spotify.search({ type: "track", query: songName }, (err, data) => {
-    console.log("data from spotify API: ", data);
-  });
+  console.log('got here')
+  spotify.search({ type: 'track', query: songName }, (err, data) => {
+    console.log('data from spotify API: ', data)
+  })
 }
 
-// Include the axios npm package
+// Include the axios npm package 
 var axios = require("axios");
 
 function searchOMDB() {
   var movieName = process.argv[3];
-  axios
-    .get(
-      "http://www.omdbapi.com/?t=" +
-        movieName +
-        "&plot=full&tomatoes=true&apikey=edfc2147"
-    )
-    .then(
-      function(response) {
-        console.log("The movie's Title is: " + JSON.stringify(response.data));
-      },
-      function(error) {
-        console.log("error");
-      }
-    );
+  axios.get("http://www.omdbapi.com/?t=" + movieName + "&plot=full&tomatoes=true&apikey=edfc2147").then(
+    function (response) {
+      console.log("The movie's Title is: " + JSON.stringify(response.data));
+    },
+    function (error) {
+      console.log('error');
+    }
+  );
 }
-function searchArtist() {
+function searchArtist(){
   var artist = process.argv[3];
 
-  axios
-    .get(
-      "https://rest.bandsintown.com/artists/" +
-        artist +
-        "/events?app_id=codingbootcamp"
-    )
-    .then(
-      function(response) {
-        console.log(
-          "The concert information: " + JSON.stringify(response.data)
-        );
-        console.log(moment(response.data[0].datetime).format("LLLL"));
-      },
-      function(error) {
-        console.log("error");
-      }
-    );
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+  function (response) {
+    console.log("The concert information: " + JSON.stringify(response.data));
+    console.log(moment(response.data[0].datetime).format('LLLL'));
+  },
+  function (error) {
+    console.log('error');
+  }
+
+);
 }
 
-fs.readFile("random.txt", function(err, data) {
-  if (err) console.log(err);
-  else console.log(data.toString());
-});
 
-var getInfo = function(data) {
+fs.readFile("random.txt", function(err,data)
+            {
+                if(err)
+                    console.log(err)
+                else
+                    console.log(data.toString());
+            });
+
+var getInfo = function (data) {
   switch (data) {
-    case "concert-this":
+    case 'concert-this':
       searchArtist();
 
-    case "spotify-this-song":
+    case 'spotify-this-song':
       searchSpotify();
 
-    case "movie-this":
+    case 'movie-this':
       searchOMDB();
 
     default:
-      return "do-what-it-says";
+    return('do-what-it-says')
   }
-};
+}
 
 console.log(getInfo(process.argv[2]));
